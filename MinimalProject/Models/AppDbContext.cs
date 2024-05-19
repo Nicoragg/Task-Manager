fc8f8f346757639195b409686b3608f53957e461
Laura Dromlewicz <dromlewiczlaura@gmail.com>
2024-05-19 19:59:50 -0300

    Alterações finais

--- MinimalProject/Models/AppDbContext.cs
+++ MinimalProject/Models/AppDbContext.cs
@@ -10,7 +10,16 @@ namespace MinimalApiProject.Models // Define o namespace do projeto.
         
         // Define uma propriedade DbSet para a entidade Usuario.
         public DbSet<Usuario> Usuarios { get; set; }
-          
+        
+        // Define uma propriedade DbSet para a entidade AtribuicaoTarefaUsuario.
+        public DbSet<AtribuicaoTarefaUsuario> Atribuicoes { get; set; }
+        
+        // Define uma propriedade DbSet para a entidade Projeto.
+        public DbSet<Projeto> Projetos { get; set; }
+        
+        // Define uma propriedade DbSet para a entidade Notificacao.
+        public DbSet<Notificacao> Notificacoes { get; set; }
+        
         // Método para configurar o contexto do banco de dados.
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -18,6 +27,24 @@ namespace MinimalApiProject.Models // Define o namespace do projeto.
             optionsBuilder.UseSqlite("Data Source=app.db");
         }
 
+        // Método para configurar a modelagem do banco de dados.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configura a chave primária composta para a entidade AtribuicaoTarefaUsuario.
+            modelBuilder.Entity<AtribuicaoTarefaUsuario>()
+                .HasKey(atu => new { atu.TarefaId, atu.UsuarioId });
 
+            // Configura o relacionamento entre AtribuicaoTarefaUsuario e Tarefa.
+            modelBuilder.Entity<AtribuicaoTarefaUsuario>()
+                .HasOne(atu => atu.Tarefa)
+                .WithMany(t => t.Atribuicoes)
+                .HasForeignKey(atu => atu.TarefaId);
+
+            // Configura o relacionamento entre AtribuicaoTarefaUsuario e Usuario.
+            modelBuilder.Entity<AtribuicaoTarefaUsuario>()
+                .HasOne(atu => atu.Usuario)
+                .WithMany(u => u.Atribuicoes)
+                .HasForeignKey(atu => atu.UsuarioId);
+        }
     }
 }
